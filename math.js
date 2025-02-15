c33af529184cf978eb47e419d0c5d88604ee4de7
Anamul Haque <138350161+Anamul9901@users.noreply.github.com>
2025-02-15 17:28:53 +0600

    Add 3 test

--- math.js
+++ math.js
@@ -2,6 +2,7 @@ export function add(numbers) {
   let sum = 0;
 
   for (const number of numbers) {
+    //  number er age + duar karone( +number ) srgin k number e convert kore felbe
     sum += +number;
   }
   return sum;
--- math.test.js
+++ math.test.js
@@ -3,10 +3,36 @@ import { add } from "./math";
 
 // it r test kaj same
 
+// 1st test
 it("should return the correct sum if an array of number is provided", () => {
   // Arrange
   const numbers = [1, 2, 3];
   const expectedResult = numbers.reduce((acc, cur) => acc + cur);
+
+  // Actions
+  const result = add(numbers);
+
+  //Assertion
+  expect(result).toBe(expectedResult);
+});
+
+// 2nd test
+it("it should provide NaN if at least one invalid number is probided", () => {
+  // Arrange
+  const numbers = [1, "any string", 3];
+
+  // Actions
+  const result = add(numbers);
+
+  //Assertion
+  expect(result).toBeNaN();
+});
+
+// 2nd test
+it("it should provide correct sum if an array numeric string is provided", () => {
+  // Arrange
+  const numbers = [1, "2", "3"];
+  const expectedResult = numbers.reduce((acc, cur) => acc + +cur);
 
   // Actions
   const result = add(numbers);
