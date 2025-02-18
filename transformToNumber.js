d9ea867b90354207f622bf1b3067d840c3845021
Anamul Haque <138350161+Anamul9901@users.noreply.github.com>
2025-02-18 20:48:08 +0600

    TransformToNumber test

---
+++ transformToNumber.js
@@ -0,0 +1,4 @@
+export function transformToNumber(value){
+    // +value, aikhane '+' duar karone numaric string k number e convert krbe
+    return +value
+}
---
+++ transformToNumber.test.js
@@ -0,0 +1,9 @@
+import { expect, it } from "vitest";
+import { transformToNumber } from "./transformToNumber";
+
+it("should return a number if a numeric string is provided", () => {
+  const stringNumber = "5";
+  const result = transformToNumber(stringNumber);
+
+  expect(result).toBeTypeOf("number");
+});
