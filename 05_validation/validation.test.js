5fc39f09535e8a41ca8cc9ce7347718ba913e262
Anamul Haque <138350161+Anamul9901@users.noreply.github.com>
2025-02-18 23:44:56 +0600

    Describe

--- 05_validation/validation.test.js
+++ 05_validation/validation.test.js
@@ -1,54 +1,61 @@
-import { it, expect } from "vitest";
+import { it, expect, describe } from "vitest";
 import { validateArrayNotEmpty, validateEmail } from "./validation";
 
-it("should validate a correct email address", () => {
-  const email = "example@gmail.com";
-  const resultFn = () => validateEmail(email);
+// describe er maddhome prteta functon er jonno eke page e alada alada vabe test lekha jai
+// for email
+describe("validateEmail()", () => {
+  it("should validate a correct email address", () => {
+    const email = "example@gmail.com";
+    const resultFn = () => validateEmail(email);
 
-  expect(resultFn).not.toThrow();
+    expect(resultFn).not.toThrow();
+  });
+
+  it("should throw an error if an invalid email is passed", () => {
+    const email = "example";
+    const resultFn = () => validateEmail(email);
+
+    expect(resultFn).toThrow("Invalid email format");
+  });
+
+  it("throws an error for an undefined email address", () => {
+    expect(() => validateEmail(undefined).toThrow("Invalid email format"));
+  });
+
+  it("throws an error for an null email address", () => {
+    expect(() => validateEmail(null).toThrow("Invalid email format"));
+  });
 });
 
-it("should throw an error if an invalid email is passed", () => {
-  const email = "example";
-  const resultFn = () => validateEmail(email);
+// for array
+describe("validateArrayNotEmpty()", () => {
+  it("should validate a non-empty array", () => {
+    const array = [2, 5];
+    const requestFn = () => validateArrayNotEmpty(array);
+    expect(requestFn).not.toThrow();
+  });
 
-  expect(resultFn).toThrow("Invalid email format");
-});
+  it("throws an error for an empty array", () => {
+    const empty_array = [];
+    const resultFn = () => validateArrayNotEmpty(empty_array);
+    expect(resultFn).toThrow("Array can not be empty.");
+  });
 
-it("throws an error for an undefined email address", () => {
-  expect(() => validateEmail(undefined).toThrow("Invalid email format"));
-});
+  it("throws an error for a not-array input", () => {
+    const non_array = "not an array";
+    const resultFn = () => validateArrayNotEmpty(non_array);
+    expect(resultFn).toThrow("Array can not be empty.");
+  });
 
-it("throws an error for an null email address", () => {
-  expect(() => validateEmail(null).toThrow("Invalid email format"));
-});
+  it("throws an error for a null input", () => {
+    const null_array = null;
+    const resultFn = () => validateArrayNotEmpty(null_array);
+    expect(resultFn).toThrow("Array can not be empty.");
+  });
 
-it("should validate a non-empty array", () => {
-  const array = [2, 5];
-  const requestFn = () => validateArrayNotEmpty(array);
-  expect(requestFn).not.toThrow();
-});
-
-it("throws an error for an empty array", () => {
-  const empty_array = [];
-  const resultFn = () => validateArrayNotEmpty(empty_array);
-  expect(resultFn).toThrow("Array can not be empty.");
-});
-
-it("throws an error for a not-array input", () => {
-  const non_array = "not an array";
-  const resultFn = () => validateArrayNotEmpty(non_array);
-  expect(resultFn).toThrow("Array can not be empty.");
-});
-
-it("throws an error for a null input", () => {
-  const null_array = null;
-  const resultFn = () => validateArrayNotEmpty(null_array);
-  expect(resultFn).toThrow("Array can not be empty.");
-});
-
-it("throws an error for an undefined input", () => {
-  const undefined_array = undefined;
-  const resultFn = () => validateArrayNotEmpty(undefined_array);
-  expect(resultFn).toThrow("Array can not be empty.");
+  it("throws an error for an undefined input", () => {
+    const undefined_array = undefined;
+    const resultFn = () => validateArrayNotEmpty(undefined_array);
+    expect(resultFn).toThrow("Array can not be empty.");
+  });
 });
